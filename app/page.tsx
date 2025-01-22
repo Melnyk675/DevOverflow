6df70dddd7e16f0dc5f4478000eaf14eadfafbfd
Dmytro Melnyk <magellan2402@gmail.com>
2025-01-22 21:15:50 +0200

    add tailwind styles

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+                                  
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-black text-primary-500">Welcome to the world of Next.js</h1>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,35 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          100: "#FFF1E6",
+          500: "#FF7000",
+        },
+        dark: {
+          100: "000000",
+          200: "#0F1117",
+          300: "#151821",
+          400: "#212734",
+          500: "#3F4354",
+        },
+        light: {
+          900: "#FFFFFF",
+          850: "#FDFDFD",
+          800: "#F4F6F8",
+          700: "#DCE3F1",
+          500: "#7B8EC8",
+          400: "#858EAD",
+        },
       },
+      boxShadow: {
+        "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
+        "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
+      },
+      screens: {
+        xs: "420px",
+        },
     },
   },
   plugins: [],
