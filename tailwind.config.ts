60d078fd8a4b0e8c9e9f73c604f9bd68c3913b9d
Dmytro Melnyk <magellan2402@gmail.com>
2025-01-23 14:38:30 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,6 +38,10 @@ export default {
       screens: {
         xs: "420px",
         },
+      fontFamily: {
+          inter: ["var(--font-inter)"],
+          "space-grotesk": ["var(--font-space-grotesk)"],
+        },
     },
   },
   plugins: [],
