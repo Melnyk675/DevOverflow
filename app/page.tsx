51edb93896e737401649afc469d20aed5310c48a
Dmytro Melnyk <magellan2402@gmail.com>
2025-01-25 20:04:20 +0200

    add dropdown menu button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,28 @@
-                                  
-export default function Home() {
-  return (
-    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-  );
-}
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+  
+      
+const Home = () => (
+  <>
+  <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+    <DropdownMenu>
+  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+</>
+)
+
+export default Home;
