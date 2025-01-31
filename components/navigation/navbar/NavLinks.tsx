5233d19408257ebdb9978e82e5da1fbd6b7abad9
Dmytro Melnyk <magellan2402@gmail.com>
2025-01-31 19:46:20 +0200

    setup MobNav and NavLinks

--- components/navigation/navbar/NavLinks.tsx
+++ components/navigation/navbar/NavLinks.tsx
@@ -6,15 +6,23 @@ import { sidebarLinks } from '@/constants';
 import Link from 'next/link';
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
+import { SheetClose } from '@/components/ui/sheet';
 
 
 const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
    const pathname = usePathname();
+   const userId = 1;
   
     return <>{sidebarLinks.map((item) => {
         const isActive = (pathname.includes(item.route) 
              && item.route.length > 1) || pathname === item.route;
             
+             if(item.route === "/profile") {
+                if (userId) item.route = `${item.route}/${userId}`;
+                else return null;
+             }
+
+
         const LinkComponent = (
           <Link 
             href={item.route} 
@@ -36,11 +44,22 @@ const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
            <p className={cn( 
             isActive ? "base-bold": "base-medium", 
             !isMobileNav && "max-lg:hidden"
-           )}>{item.label}</p>
-          </Link>
+           )}
+           >
+           {item.label}
+          </p>
+         </Link>
         );
 
-        return LinkComponent;
+        return isMobileNav ? (
+          <SheetClose asChild key={item.route}>
+           {LinkComponent}
+          </SheetClose>
+        ) : (
+        <React.Fragment key={item.route}>
+            {LinkComponent}
+        </React.Fragment>
+        );
     })}
    </>;
   };
