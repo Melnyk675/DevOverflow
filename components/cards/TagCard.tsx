3426976cfafa1b4319641348be65cd55a4a7b258
Dmytro Melnyk <magellan2402@gmail.com>
2025-02-04 23:14:17 +0200

    add Devicon (set of icons)

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,6 +1,8 @@
 import ROUTES from '@/constants/routes';
 import Link from 'next/link';
 import React from 'react';
+import { Badge } from '../ui/badge';
+import { getDeviconClassName } from '@/lib/utils';
 
 interface Props {
     _id: number;
@@ -11,12 +13,25 @@ interface Props {
 }
 
 const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
+   const iconClass = getDeviconClassName(name);
+
   return (
     <Link
       href={ROUTES.TAGS(_id)}
       className='flex justify-between gap-2'
       >
-        {name}
+        <Badge className='subtle-medium background-light800_dark300
+          text-light400_light500 rounded-md border-none px-4 py-2
+          uppercase'>
+          <div className='flex-center space-x-2'>
+            <i className=''>{`${iconClass} text-sm`}</i>
+            <span>{name}</span>
+          </div>
+        </Badge>
+
+        {showCount && (
+            <p className='small-medium text-dark-500_light700'>{questions}</p>
+        )}
      </Link>
   )
 }
