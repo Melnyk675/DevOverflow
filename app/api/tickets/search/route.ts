048baa6558fbcf80a9ab3655e49706ec4ba9b41b
Dmytro Melnyk <magellan2402@gmail.com>
2025-02-22 21:45:49 +0200

    testing route handlers

---
+++ app/api/tickets/search/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+import tickets from "@/database";
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("query");
+
+    if(!query) return NextResponse.json(tickets);
+
+    const filteredTickets = tickets.filter((ticket) => ticket.name.toLowerCase().includes(query.toLowerCase()));
+
+    return NextResponse.json(filteredTickets);
+}
---
+++ database.ts
@@ -0,0 +1,52 @@
+const tickets = [
+    {
+      id: 1,
+      name: "Fix Next.js issue",
+      status: "Open",
+      type: "Bug",
+    },
+    {
+      id: 2,
+      name: "Implement new feature",
+      status: "In Progress",
+      type: "Feature",
+    },
+    {
+      id: 3,
+      name: "Refactor code",
+      status: "Done",
+      type: "Improvement",
+    },
+    {
+      id: 4,
+      name: "Optimize performance",
+      status: "Open",
+      type: "Performance",
+    },
+    {
+      id: 5,
+      name: "Add new feature",
+      status: "In Progress",
+      type: "Feature",
+    },
+    {
+      id: 6,
+      name: "Fix bug",
+      status: "Done",
+      type: "Bug",
+    },
+    {
+      id: 7,
+      name: "Implement new feature",
+      status: "Open",
+      type: "Feature",
+    },
+    {
+      id: 8,
+      name: "Optimize performance",
+      status: "In Progress",
+      type: "Performance",
+    }
+]
+
+export default tickets;
