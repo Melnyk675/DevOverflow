f3e7f1076f51a3ef045ef906c2400a7165345598
Dmytro Melnyk <magellan2402@gmail.com>
2025-03-02 20:37:26 +0200

    fixing api route

--- constants/routes.ts
+++ constants/routes.ts
@@ -6,6 +6,7 @@ const ROUTES = {
     PROFILE: (id: number) => `/profile/${id}`,
     QUESTION: (id: number) => `/question/${id}`,
     TAGS: (id: number) => `/tags/${id}`,
+    SIGN_IN_WITH_OAUTH: 'signin-with-oauth',
 }
 
 export default ROUTES;
