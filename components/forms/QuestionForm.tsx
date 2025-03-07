d9dff82766e9ce03fc944d5672e2fdd2021ac074
Dmytro Melnyk <magellan2402@gmail.com>
2025-03-07 23:17:41 +0200

    implement Create Question Form

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,23 +1,31 @@
 "use client";
 
-import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import dynamic from 'next/dynamic';
-import { AskQuestionSchema } from '@/lib/validations';
+import React, { useRef, useTransition } from 'react';
+import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { MDXEditorMethods } from '@mdxeditor/editor';
+import { ReloadIcon } from "@radix-ui/react-icons";
+
+import { AskQuestionSchema } from '@/lib/validations';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
-import { MDXEditorMethods } from '@mdxeditor/editor';
-import { z } from 'zod';
 import TagCard from '../cards/TagCard';
+import { createQuestion } from '@/lib/actions/question.action';
+import { toast } from '@/hooks/use-toast';
+import { useRouter } from 'next/navigation';
+import ROUTES from '@/constants/routes';
 
 const Editor = dynamic(() => import('@/components/editor'), {
     ssr: false
   })
 
 const QuestionForm = () => {
+   const router = useRouter();
    const editorRef = useRef<MDXEditorMethods>(null);
+   const [isPending, startTransition] = useTransition();
 
    const form = useForm<z.infer<typeof AskQuestionSchema>>({
       resolver: zodResolver(AskQuestionSchema),
@@ -66,8 +74,27 @@ const QuestionForm = () => {
     }
   };
 
-  const handleCreateQuestion = (data: z.infer<typeof AskQuestionSchema>) => {
-    console.log(data);
+  const handleCreateQuestion = async (
+    data: z.infer<typeof AskQuestionSchema>
+  ) => {
+    startTransition(async () => {
+      const result = await createQuestion(data);
+
+    if (result.success) {
+      toast({
+        title: "Success",
+        description: "Question created successfully"
+      });
+
+      if (result.data) router.push(ROUTES.QUESTION(result.data._id));
+    } else {
+      toast({
+        title: `Error ${result.status}`,
+        description: result.error?.message || "Failed to create question",
+        variant: "destructive",
+      });
+     } 
+    });
   };
 
   return <Form {...form}>
@@ -172,13 +199,22 @@ const QuestionForm = () => {
 
           <div className='mt-16 flex justify-end'>
             <Button 
-              type="submit" 
-              className='primary-gradient !text-light-900 w-fit'>
-            Ask A Question
+              type="submit"
+              disabled={isPending} 
+              className='primary-gradient !text-light-900 w-fit'
+             >
+                {isPending ? (
+                  <>
+                    <ReloadIcon className='mr-2 size-4 animate-spin' />
+                    <span>Submitting</span>
+                  </>
+                ) : (
+                  <>Ask a question</>
+                )}
             </Button>
           </div>
        </form>
-  </Form>;
-}
+    </Form>
+  };
 
 export default QuestionForm;
