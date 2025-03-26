b909b3a29016f0bb554617f7be26e0b3fd5d07a2
Dmytro Melnyk <magellan2402@gmail.com>
2025-03-27 00:04:47 +0200

    implement Get all Answers Action

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -11,6 +11,7 @@ import { formatNumber, getTimeStamp } from '@/lib/utils';
 import { Preview } from '@/components/editor/Preview';
 import { getQuestion, incrementViews } from '@/lib/actions/question.action';
 import AnswerForm from '@/components/forms/AnswerForm';
+import { getAnswers } from '@/lib/actions/answer.action';
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -21,10 +22,20 @@ const QuestionDetails = async ({ params }: RouteParams) => {
 
   })
 
-
- 
-
   if (!success || !question) return redirect("/404");
+
+  const { 
+    success: answersLoaded, 
+    data: answersResult, 
+    error: answersError 
+  } = await getAnswers({
+    questionId: id,
+    page: 1,
+    pageSize: 10,
+    filter: "latest"
+  });
+
+  console.log("Answers", answersResult)
 
   const { author, createdAt, answers, views, tags, content, title } = question;
 
