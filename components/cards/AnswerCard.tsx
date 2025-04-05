0c009e217e46174a16f8469c6de7498108bfc9a4
Dmytro Melnyk <magellan2402@gmail.com>
2025-04-05 14:33:57 +0300

    implement Answer Votes

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -1,11 +1,25 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { Suspense } from 'react';
 import UserAvatar from '../UserAvatar';
 import ROUTES from '@/constants/routes';
 import { getTimeStamp } from '@/lib/utils';
 import { Preview } from '../editor/Preview';
+import Votes from '../votes/Votes';
+import { hasVoted } from '@/lib/actions/vote.action';
 
-const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
+const AnswerCard = ({ 
+  _id, 
+  author, 
+  content, 
+  createdAt,
+  upvotes,
+  downvotes, 
+}: Answer) => {
+  const hasVotedPromise = hasVoted({ 
+    targetId: _id, 
+    targetType: "answer", 
+  });
+
   return (
     <article className='light-border border-b py-10'>
       <span id={JSON.stringify(_id)} className='hash-span'/>
@@ -36,7 +50,17 @@ const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
         </Link>
          </div>
 
-         <div className='flex justify-end'>Votes</div>
+         <div className='flex justify-end'>
+           <Suspense fallback={<div>Loading...</div>}>
+             <Votes 
+               targetType="answer"
+               targetId={_id}
+               upvotes={upvotes}
+               downvotes={downvotes}
+               hasVotedPromise={hasVotedPromise}
+             />
+           </Suspense>
+         </div>
       </div>
 
       <Preview 
