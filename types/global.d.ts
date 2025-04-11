ed88a40f2dd08c94d8acffae89ecac39d32b5a53
Dmytro Melnyk <magellan2402@gmail.com>
2025-04-11 19:15:00 +0300

    implement Get All Users Action

--- types/global.d.ts
+++ types/global.d.ts
@@ -66,3 +66,17 @@ interface Answer {
     author: string | Author;
     question: Question;
   }
+
+  interface User {
+    _id: string;
+    name: string;
+    username: string;
+    email: string;
+    bio?: string;
+    image?: string;
+    location?: string;
+    portfolio?: string;
+    reputation?: number;
+    createdAt: Date;
+  }
+  
