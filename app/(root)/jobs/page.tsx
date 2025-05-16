18c2b1ad66bffd1ac4d055164a4dddb8b2ffc223
Dmytro Melnyk <magellan2402@gmail.com>
2025-05-16 21:10:36 +0300

    implement Job Filter

--- app/(root)/jobs/page.tsx
+++ app/(root)/jobs/page.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import JobsFilter from "@/components/filters/JobFilter";
+import { fetchCountries, fetchJobs, fetchLocation } from "@/lib/actions/job.action";
 
-const Jobs = () => {
+
+const Jobs = async ({ searchParams }: RouteParams) => {
+  const { query, location, page } = await searchParams;
+  const userLocation = await fetchLocation();
+
+  const jobs = await fetchJobs({
+    query: `${query}, ${location}` || `Software Engineer in ${userLocation}`,
+    page: page ?? 1,
+  });
+
+  const countries = await fetchCountries();
+  const parsedPage = parseInt(page ?? 1);
+
+  console.log(jobs);
+
   return (
-    <div>Jobs</div>
+    <>
+      <h1 className="h1-bold text-dark100_light900">Jobs</h1>
+
+      <div className="flex">
+        <JobsFilter countriesList={countries} />
+      </div>
+    </>
   )
 }
 
